b69aa22ee4dd18dbd1d014812381329d5e980aac
RomaSorokivskiy <sorokivroma@gmail.com>
2023-10-26 04:56:28 +0300

    again little change!!!!!!

--- app/components/crudContainer.component.tsx
+++ app/components/crudContainer.component.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {FetchCrudList, RemoveCrud} from "app/app/reducers/Action";
 import {useEffect, useState} from "react";
 import {connect} from "react-redux";
--- app/reducers/Store.ts
+++ app/reducers/Store.ts
@@ -1,6 +1,8 @@
 import { configureStore,combineReducers } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 import { Reducer } from "./Reducer";
 
 const rootreducer=combineReducers({crud:Reducer});
-const Store=configureStore({reducer:rootreducer,})
+const Store=configureStore({reducer:rootreducer,middleware:[thunk,logger]})
 export default Store;
