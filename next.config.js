a16c2ec396a4e2a52436408f2c80e5d06d70492b
RomaSorokivskiy <sorokivroma@gmail.com>
2023-10-26 04:36:59 +0300

    again little change!!!!!!

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    redirects: async() => {
+        return [
+            {
+                source: '/auths',
+                destination: '/table',
+                permanent: true
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
