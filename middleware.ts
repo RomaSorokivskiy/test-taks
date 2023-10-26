455f35d02424811d1ef25f92e6e24bde8b7d49f6
RomaSorokivskiy <sorokivroma@gmail.com>
2023-10-26 03:56:28 +0300

    again little change!

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth(
+    // `withAuth` augments your `Request` with the user's token.
+    function middleware(req) {
+        console.log(req.nextauth.token)
+    },
     {
         callbacks: {
             authorized: ({ token }) => {
@@ -10,7 +14,7 @@ export default withAuth(
         },
         secret: process.env.NEXTAUTH_SECRET,
     }
-);
+)
 export const config = {
     matcher: ["/table"],
 };
