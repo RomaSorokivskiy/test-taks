ebb54fc818adbada64cfa6972a2b09e60414ff80
RomaSorokivskiy <sorokivroma@gmail.com>
2023-10-26 03:44:35 +0300

    again little change!

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,5 @@
-export { default } from 'next-auth/middleware'
-
+import { withAuth } from "next-auth/middleware";
+export default withAuth({
+    secret: process.env.SECRET,
+});
 export const config = { matcher: ['/profile','/table'] }
